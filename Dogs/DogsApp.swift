27d426eefe2c8b8fc96050c9bcb9e6be914829cc
Morgan Pritchard <morgan.pritchard@fhnw.edu>
2026-02-24 09:17:55 -0700

    Initial Commit

---
+++ Dogs/DogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogsApp.swift
+//  Dogs
+//
+//  Created by Teaching on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct DogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
